1385d0057afb056bf8b3970461e93bb66a4b3a35
Valery <i@vvaganov.ru>
2024-07-01 17:31:59 +0300

    KTB-06 implemented the output of statistics on the number of learned words and in % of the total number

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,18 +13,25 @@ fun main() {
         dictionary.add(Word(split[0], split[1], split[2].toIntOrNull() ?: 0))
     }
 
-    while (true){
-        println("Меню: \n" +
-                "1 – Учить слова\n" +
-                "2 – Статистика\n" +
-                "0 – Выход")
+    while (true) {
+        println(
+            "Меню: \n" +
+                    "1 – Учить слова\n" +
+                    "2 – Статистика\n" +
+                    "0 – Выход"
+        )
         val answer = readln().toInt()
-        when(answer){
+        when (answer) {
             1 -> println("Нажата кнопка 1")
-            2 -> println("Нажата кнопка 2")
+            2 -> {
+                val learnedWordsListSize = dictionary.filter { it.correctAnswersCount >= 3 }.size
+                println(
+                    "Выучено $learnedWordsListSize из ${dictionary.size} " +
+                            "слов | ${(learnedWordsListSize / dictionary.size.toDouble() * 100).toInt()}%"
+                )
+            }
             0 -> break
             else -> println("Выберете корректный пункт меню")
         }
     }
 }
-

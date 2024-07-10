124aa019ce470d456342944f5b5deb9bb29421b9
Valery <i@vvaganov.ru>
2024-07-10 09:06:00 +0300

    KTB-12 renamed the getMe variable

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -6,13 +6,13 @@ import java.net.http.HttpResponse
 fun main(args: Array<String>) {
 
     val botToken = args[0]
-    val urlGetMi = "https://api.telegram.org/bot$botToken/getMe"
+    val urlGetMe = "https://api.telegram.org/bot$botToken/getMe"
     val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates"
 
     val client: HttpClient = HttpClient.newBuilder().build()
-    var request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetMi)).build()
+    var request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetMe)).build()
     var response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
-    println("getMy - ${response.body()}")
+    println("getMe - ${response.body()}")
     request = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
     response = client.send(request, HttpResponse.BodyHandlers.ofString())
     println("getUpdates - ${response.body()}")

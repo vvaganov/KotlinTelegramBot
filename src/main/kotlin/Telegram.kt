dfc843f37fd668d638ea357f62c2e24c8a4e6d4e
Valery <i@vvaganov.ru>
2024-07-16 14:18:08 +0300

    KTB-16 implemented menu buttons, and button click parsing

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,26 +1,43 @@
+import org.example.LearnWordsTrainer
 import java.net.URI
+import java.net.URLEncoder
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.nio.charset.StandardCharsets
 
 fun main(args: Array<String>) {
     val botToken = args[0]
     var updateId = 0
     val updateIdRegex: Regex = "\"update_id\":(\\d+)".toRegex()
     val chatIdRegex: Regex = "\"chat\":\\{\"id\":(\\d+)".toRegex()
-    val messageText: Regex = "\"text\":\"(.+?)\"".toRegex()
+    val messageRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
+    val dataRegex: Regex = "\"data\":\"(.+?)\"".toRegex()
+
+    val trainer = LearnWordsTrainer()
 
     while (true) {
         Thread.sleep(2000)
         val updates = getUpdate(botToken, updateId)
+        println(updates)
         val valueId = updateIdRegex.find(updates)?.groups?.get(1)?.value?.toIntOrNull() ?: continue
         updateId = valueId + 1
         val chatId = chatIdRegex.find(updates)?.groups?.get(1)?.value?.toInt()
-        val message = messageText.find(updates)?.groups?.get(1)?.value
+        val message = messageRegex.find(updates)?.groups?.get(1)?.value
+        val data = dataRegex.find(updates)?.groups?.get(1)?.value
 
-        if (message?.lowercase() == "hello" && chatId != null){
+
+        if (message?.lowercase() == "hello" && chatId != null) {
             sendMessage(botToken, chatId, "Hello")
         }
+
+        if (message?.lowercase() == "/start" && chatId != null) {
+            sendMenu(botToken, chatId)
+        }
+        if (data?.lowercase() == "statistic_clicked" && chatId != null) {
+            sendMessage(botToken, chatId, "Выучено 10 из 100 слов!!!")
+        }
+
     }
 }
 
@@ -30,12 +47,48 @@ fun getUpdate(botToken: String, updateId: Int): String {
     val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
     val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
     return response.body()
-    }
+}
 
 fun sendMessage(botToken: String, chatId: Int, message: String): String {
-    val urlSendMessage = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$message"
+    val encoded = URLEncoder.encode(message, StandardCharsets.UTF_8)
+    val urlSendMessage = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$encoded"
     val client: HttpClient = HttpClient.newBuilder().build()
     val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlSendMessage)).build()
+    val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
+    return response.body()
+}
+
+fun sendMenu(botToken: String, chatId: Int): String {
+    val sendMenuBody = """
+        {
+    "chat_id": $chatId,
+    "text": "Основное меню",
+    "reply_markup": {
+       "inline_keyboard": [
+            [
+                {
+                    "text": "Изучать слова",
+                    "callback_data": "learn_word_clicked"
+                },
+                {
+                    "text": "Статистика",
+                    "callback_data": "statistic_clicked"
+                },
+                {
+                    "text": "Выход",
+                    "callback_data": "exit_clicked"
+                }
+            ]
+        ]
+    }
+}
+    """.trimIndent()
+    val urlSendMessage = "https://api.telegram.org/bot$botToken/sendMessage"
+    val client: HttpClient = HttpClient.newBuilder().build()
+    val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlSendMessage))
+        .header("Content-type", "application/json")
+        .POST(HttpRequest.BodyPublishers.ofString(sendMenuBody))
+        .build()
     val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
     return response.body()
 }
--- src/main/kotlin/Word.kt
+++ src/main/kotlin/Word.kt
@@ -1,7 +1,2 @@
 package org.example
 
-data class Word(
-    val original: String,
-    val translate: String,
-    var correctAnswersCount: Int = 0,
-)
